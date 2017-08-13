54db880b99579bd729222ee293506b04cbf76250
Alfred <alfred@bufferapp.com>
2017-08-13 14:53:27 +0800

    Add theme css and js functions

--- functions.php
+++ functions.php
@@ -1,12 +1,24 @@
 <?php
 
-function wpt_theme_styles() {
+// Load Theme CSS
+function theme_styles() {	
 
-     wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css' );
-     wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
-     wp_enqueue_style( 'normalize_css', 'http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' );
-     wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css' );
+    wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
+    wp_enqueue_style( 'normalize_css', 'http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' );
+	wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/css/bootstrap.css' );	
+	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );	
+
 }
-add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
+add_action( 'wp_enqueue_scripts', 'theme_styles' );
+
+// Load Theme JS
+function theme_js() {
+	
+	wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/js/bootstrap.js', array('jquery'), '', true );
+	wp_enqueue_script( 'theme_js', get_template_directory_uri() . '/js/theme.js', array('jquery','bootstrap_js'), '', true );
+
+}
+add_action( 'wp_enqueue_scripts', 'theme_js' );
 
 ?>
