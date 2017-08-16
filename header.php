99239bccbcbfe4367ddc664c5e274018c8c8dd5a
Alfred <alfred@bufferapp.com>
2017-08-16 21:28:01 +0800

    Add social links

--- header.php
+++ header.php
@@ -30,8 +30,8 @@
         <nav class="blog-nav">
           <a class="blog-nav-item" href="#">About</a>
           <a class="blog-nav-item active" href="#">Journal</a>
-          <a class="blog-nav-item" href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/twitter-logo.png" alt="Twitter" width="25px" height="25px"></a>
-          <a class="blog-nav-item" href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/linkedin-logo.png" alt="LinkedIn" width="25px" height="25px"></a>
+          <a class="blog-nav-item" href="https://twitter.com/alfred_lua"><img src="<?php echo get_template_directory_uri(); ?>/images/twitter-logo.png" alt="Twitter" width="25px" height="25px"></a>
+          <a class="blog-nav-item" href="https://www.linkedin.com/in/alfredlua/"><img src="<?php echo get_template_directory_uri(); ?>/images/linkedin-logo.png" alt="LinkedIn" width="25px" height="25px"></a>
         </nav>
       </div>
     </div>
