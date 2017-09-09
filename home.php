bc6023b9c9a421c73816b41f8bbcb40321986a7d
Alfred <alfred@bufferapp.com>
2017-09-09 17:35:04 +0800

    Remove hero from home

--- home.php
+++ home.php
@@ -2,30 +2,6 @@
 
     <div class="container" role="main">
 
-    	<?php if(!is_paged()): ?>
-
-	    	<div class="hero">
-	    		<div class="hero-photo">
-	    			<img src="<?php echo get_template_directory_uri(); ?>/images/profile-photo.png" alt="Alfred Lua's profile photo" width="500">
-	    		</div>
-
-	    		<div class="hero-intro">
-		    		<h2>
-		    			Hi, I’m Alfred Lua.<br><br>
-
-			    		I’m a Content Crafter at <a href="https://buffer.com">Buffer</a>, and I write for the <a href="https://blog.bufferapp.com/">Buffer blog</a>.<br><br>
-
-			    		<!--- I write about content marketing, social media marketing, and SEO.<br><br> -->
-
-			    		Here are my latest posts…
-		    		</h2>
-	    		</div>	
-	    	</div>
-
-	    <?php endif; ?>	
-
-    	<div class="clear"></div>
-
 	    <div class="row">
 
 			<?php 
