74564d46686344c344bac82ab993f346cc0ba9ff
Alfred <alfred@bufferapp.com>
2017-11-05 15:01:36 +0800

    Create page template

---
+++ content-page.php
@@ -0,0 +1,12 @@
+<div class="blog-post">
+	<div class="page-header">
+		<h1 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+	</div>
+
+	<div class="blog-post-content">
+
+		<?php the_content(); ?>
+
+	</div>
+
+</div>
---
+++ page.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+
+    <div class="container" role="main">
+
+	    <div class="row">
+
+			<?php 
+			if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+
+				get_template_part( 'content-page', get_post_format() );
+
+			?>
+
+			<hr> 
+
+			<?php endwhile; else: ?>
+
+				<div class="page-header">	
+			    	<h1>Oh no!</h1>
+			    </div><!-- page-header -->
+
+			    <p>We could not find this page!!!</p>
+
+			<?php endif; ?>
+
+	    </div>
+
+    </div><!-- .container -->
+
+<?php get_footer(); ?>
