f3906d3f2ef8e96345f571efa8f959ab5b2c9548
Alfred <alfred@bufferapp.com>
2018-01-23 10:14:41 +0800

    Increase subheading font weight

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ function alfmin_styles() {
 
 	wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css' );
     wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
-    wp_enqueue_style( 'normalize_css', 'http://fonts.googleapis.com/css?family=Lora|Open+Sans:100,400,italic' );
+    wp_enqueue_style( 'normalize_css', 'http://fonts.googleapis.com/css?family=Lora|Open+Sans:100,400,600,italic' );
 	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );	
 
 }
